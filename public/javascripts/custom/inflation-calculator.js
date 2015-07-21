9698485ceb84392715001ee21d8e8f1fe33074ef
elainapolson <elainapolson@gmail.com>
2015-07-21 14:17:22 -0400

    done

--- public/javascripts/custom/inflation-calculator.js
+++ public/javascripts/custom/inflation-calculator.js
@@ -1,1 +1,34 @@
 // code your functions here
+
+function fetchOptions(){
+  return {
+    start: $('#startDate').val(),
+    end: $('#endDate').val(),
+    amount: $('#startPrice').val()
+  }
+}
+
+function addPriceToPage(amount) {
+  var amount = amount.replace("$", "");
+  $('#endPrice').val(amount);
+  return amount;
+}
+
+// callback is addPricetoPage
+function priceFor(options, callback) {
+   var apiUrl = 'http://www.statbureau.org/calculate-inflation-price-jsonp?jsoncallback=?';
+
+  $.getJSON(apiUrl, {
+      country: 'united-states',
+      start: options.start,
+      end: options.end,
+      amount: options.amount,
+      format: true
+  })
+    .done(callback);
+}
+
+// callback is addPricetoPage
+function fetchEndPrice(callback) {
+  priceFor(fetchOptions(), callback);
+}
--- public/javascripts/custom/on-click.js
+++ public/javascripts/custom/on-click.js
@@ -1,7 +1,9 @@
 "use strict";
 
 $(document).ready(function() {
-
-  // add your on click event handler here
   
+  $('#calc').click(function() {
+    fetchEndPrice(addPriceToPage);
+  });
+
 });
